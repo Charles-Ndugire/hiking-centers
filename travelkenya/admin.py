120b91bc6f0b4d998528e14e82c45d2f7a434db8
Charles-Ndugire <71345331+Charles-Ndugire@users.noreply.github.com>
2021-03-24 17:40:18 +0300

    Update admin.py

--- travelkenya/admin.py
+++ travelkenya/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Enthusiast,Article,Location
 
 class ArticleAdmin(admin.ModelAdmin):
-    filter_horizontal =('location',)
+    filter_horizontal = ('location',)
 
 admin.site.register(Enthusiast)
 admin.site.register(Article,ArticleAdmin)
