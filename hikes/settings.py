698da16ef2b3b3e36e5e41396fd3284e782ced3b
Charles-Ndugire <71345331+Charles-Ndugire@users.noreply.github.com>
2021-03-24 18:06:27 +0300

    switching DEBUG = True fro false

--- hikes/settings.py
+++ hikes/settings.py
@@ -69,9 +69,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'vetdaily.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
